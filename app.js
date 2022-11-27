56ab3d3575e9f402625c4f58627b23d0e2673519
Leonard Lai <lailisheng@outlook.sg>
2022-11-27 21:27:50 +0800

    added new function

--- app.js
+++ app.js
@@ -7,5 +7,31 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    let pSelection = playerSelection.toLowerCase();
+    let cSelection = computerSelection;
+
+    if ( pSelection == 'rock' && computerSelection == 'paper' ) {
+        return 'You Lose! Paper beats Rock'
+    }
+    else if ( pSelection == 'rock' && computerSelection == 'scissors' ) {
+        return 'You Win! Rock beats Scissors'
+    }
+
+    else if ( pSelection == 'paper' && computerSelection == 'rock' ) {
+        return 'You Win! Paper beats Rock'
+    }
+    else if ( pSelection == 'paper' && computerSelection == 'scissors' ) {
+        return 'You Lose! Scissors beats Paper'
+    }
+
+    else if ( pSelection == 'scissors' && computerSelection == 'paper' ) {
+        return 'You Win! Scissors beats Paper'
+    }
+    else if ( pSelection == 'scissors' && computerSelection == 'rock' ) {
+        return 'You Lose! Rock beats Scissors'
+    }
+
+    else {
+        return "It's a draw!"
+    }
 }
