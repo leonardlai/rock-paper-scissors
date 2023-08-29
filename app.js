9dbd8d1200cd56f93f95d455f777b35f27fe436b
Leonard Lai <lailisheng@outlook.sg>
2023-08-29 21:01:28 +0800

    added UI solution

--- app.js
+++ app.js
@@ -1,4 +1,50 @@
+//document.getElementsByTagName("button").style.fontSize = "6vw";
 const choices = [ 'rock', 'paper', 'scissors' ];
+
+function printResult() {
+    
+}
+
+function getBothChoice() {
+    const btns = document.querySelectorAll('button');
+    let playerScore = 0 , computerScore = 0; 
+
+    btns.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            const playerChoice = btn.id;
+            const cSelection = getComputerChoice();
+            const stringResult = playRound(playerChoice, cSelection);
+
+            console.log(`You selected ${playerChoice}, Computer selected ${cSelection}`);
+            console.log(stringResult);
+            if (stringResult.startsWith("Win",4)) {
+                playerScore = playerScore + 1; 
+            } else if (stringResult.startsWith("Lose",4)) {
+                computerScore = computerScore + 1; 
+            }        
+            console.log(`Player:${playerScore}, Computer:${computerScore}`);
+
+            // your JavaScript file
+            const container = document.querySelector('#result');
+
+            const content = document.createElement('div');
+            content.classList.add('content');
+            content.textContent = `You selected ${playerChoice}, Computer selected ${cSelection}. \n ${stringResult} \nPlayer:${playerScore}, Computer:${computerScore}`;
+
+            container.appendChild(content);
+
+            if (playerScore === 5) {
+                const winner = document.createElement('h2');
+                winner.classList.add('content');
+                winner.textContent = `Winner is player`;
+
+                container.appendChild(winner);
+
+
+            }
+        });
+    });
+}
 
 function getComputerChoice() {
     
@@ -6,38 +52,28 @@ function getComputerChoice() {
     return choices[random]
 }
 
-function calculateScore(stringResult) {
-        
-    if (stringResult.startsWith("Win",4)) {
-        return playerScore + 1; 
-    } else if (stringResult.startsWith("Lose",4)) {
-        return computerScore + 1; 
-    }   
-}
-
 function playRound(playerSelection, computerSelection) {
-    let pSelection = playerSelection.toLowerCase();
-    let cSelection = computerSelection;
+    let pSelection = playerSelection;
 
     if ( pSelection == 'rock' && computerSelection == 'paper' ) {
         return 'You Lose! Paper beats Rock'
     }
     else if ( pSelection == 'rock' && computerSelection == 'scissors' ) {
-        return 'You Win! Rock beats Scissors'
+        return 'You Win! Rock beats Scissor'
     }
 
     else if ( pSelection == 'paper' && computerSelection == 'rock' ) {
         return 'You Win! Paper beats Rock'
     }
     else if ( pSelection == 'paper' && computerSelection == 'scissors' ) {
-        return 'You Lose! Scissors beats Paper'
+        return 'You Lose! Scissor beats Paper'
     }
 
-    else if ( pSelection == 'scissors' && computerSelection == 'paper' ) {
-        return 'You Win! Scissors beats Paper'
+    else if ( pSelection == 'scissor' && computerSelection == 'paper' ) {
+        return 'You Win! Scissor beats Paper'
     }
-    else if ( pSelection == 'scissors' && computerSelection == 'rock' ) {
-        return 'You Lose! Rock beats Scissors'
+    else if ( pSelection == 'scissor' && computerSelection == 'rock' ) {
+        return 'You Lose! Rock beats Scissor'
     }
 
     else {
@@ -47,30 +83,24 @@ function playRound(playerSelection, computerSelection) {
 
 
 function game() {
-    const MAX_ROUND = 5;
-    let playerScore = 0, computerScore = 0;
 
-    for (let i = 0 ; i < MAX_ROUND ; i++ ) {        
-
-        let playInput = window.prompt(`Game ${i+1}: Enter Rock or Scissor or Paper`);
-        const cSelection = getComputerChoice();
-
-        console.group(`Game ${i+1}`);
-        console.log(`You selected ${playInput}, Computer selected ${cSelection}`);
-        console.log(playRound(playInput, cSelection));
-
-        const stringResult = playRound(playInput, cSelection);
+    let playerScore = 0, computerScore = 0;     
+    
+    const stringResult =  getBothChoice();
+    while(stringResult) {
         if (stringResult.startsWith("Win",4)) {
             playerScore = playerScore + 1; 
         } else if (stringResult.startsWith("Lose",4)) {
             computerScore = computerScore + 1; 
         }        
         console.log(`Player:${playerScore}, Computer:${computerScore}`);
-        
     }
+
     const winner = playerScore > computerScore ? "Player Wins" :
     computerScore > playerScore ? "Computer Wins" : 
     "It's a Draw!"
     console.log(winner);
 
 }
+
+
